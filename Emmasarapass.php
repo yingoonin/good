b09aa25343eac7c8805569725cd461a5af89b103
adiwalfare <ronimawiads@gmail.com>
2025-07-09 23:07:15 +0500

    Update Emmasarapass.php

--- Emmasarapass.php
+++ Emmasarapass.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'EMMA SARA ';
+        $mail->Subject = 'EMMA SARA';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
