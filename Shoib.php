b7d20160959574ce579e7ef2868c444869230066
adiwalfare <ronimawiads@gmail.com>
2025-07-09 21:02:44 +0500

    Update Shoib.php

--- Shoib.php
+++ Shoib.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("https://shoib-p.vercel.app/");
+header("Location: https://shoib-p.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
